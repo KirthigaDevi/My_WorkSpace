357bc83d2c2c5be9a6d6826d9d2b736a2c12ed65
KirthigaDevi <108298540+KirthigaDevi@users.noreply.github.com>
2022-07-01 19:44:53 +0530

    Update ParallelExecution.java

--- ParallelExecution.java
+++ ParallelExecution.java
@@ -116,11 +116,12 @@
 
 			public void Safari() {
 			
-				System.setProperty("webdriver.gecko.driver", "C:\\eclipse\\drivers\\geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver", "C:\\eclipse\\drivers\\geckodriver.exe");
 			WebDriver driver = new FirefoxDriver();
 			driver.navigate().to("https://www.amazon.in/");
 			driver.manage().window().maximize();
-		}
+		System.out.println("***Login Successfull in Safari***");
+			}
 	}
 
 
